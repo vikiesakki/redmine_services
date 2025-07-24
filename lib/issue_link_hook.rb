362731d82ae5b56244775677df9780c9f1c02b8d
Vignesh <vemcse@gmail.com>
2025-07-24 15:40:36 +0530

    Issue add func

--- lib/issue_link_hook.rb
+++ lib/issue_link_hook.rb
@@ -2,5 +2,5 @@ class IssueLinkHook < Redmine::Hook::ViewListener
 	render_on :view_issues_show_description_bottom,
                 partial: 'hooks/issue_share_link'
     render_on :view_issues_form_details_bottom,
-    			partial: 'hooks/_issue_add_func'
+    			partial: 'hooks/issue_add_func'
 end
